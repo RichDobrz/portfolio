e71d5bfdb036ae0475b64f35d560da37ba7f19f8
RichDobrz <rjdobrz@gmail.com>
2019-01-28 15:46:05 -0500

    Nav menu

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import { Router } from "react-router-dom"
+import Home from "./components/Home/Home"
+import Nav from "./components/Nav/Nav"
+import { BrowserRouter as Router, Route} from "react-router-dom"
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      <Router>
+          <div className="App">
+            
+            <header className="App-header">
+              <Nav />
+            </header>
+
+          <div className="App-Main">
+            <Route path="/" component={Home} />
+          </div>
+        
+        </div>
+      </Router>
+
+        
+
+
+    )
   }
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import "./Nav.css"
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <p>Test</p>
+
+    )
+  }
+
+
+}
+
+export default Nav
